ac288940a327d72c18aa6cfebda3c992a7b8f357
alexa <alexa@DESKTOP-J22173Q>
2021-03-15 15:34:53 +0100

    correction derniers coding foutu dans utilisateur controller erreur et
    remis dans livre controller

--- bibliotheque api/src/main/java/com/bibliotheque/api/controller/LivreController.java
+++ bibliotheque api/src/main/java/com/bibliotheque/api/controller/LivreController.java
@@ -1,5 +1,20 @@
 package com.bibliotheque.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bibliotheque.api.model.Livre;
+import com.bibliotheque.api.service.LivreService;
+
+@RestController
 public class LivreController {
+	@Autowired
+	private LivreService livreService;
+	
+	@GetMapping("/Livres")
+	public Iterable<Livre> getLivre(){
+		return livreService.getLivres();
+	}
 
 }
