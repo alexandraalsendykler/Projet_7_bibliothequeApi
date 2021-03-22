af787ec42c28523b463ab6ad6b6a94c868cb72c4
alexa <alexa@DESKTOP-J22173Q>
2021-03-22 19:29:15 +0100

    change

--- bibliotheque api/src/main/java/com/bibliotheque/api/controller/UtilisateurController.java
+++ bibliotheque api/src/main/java/com/bibliotheque/api/controller/UtilisateurController.java
@@ -3,8 +3,9 @@ package com.bibliotheque.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,9 @@ public class UtilisateurController {
 	@Autowired
 	private UtilisateurService utilisateurService;
 
-	@PostMapping("/seconnecter") // changer postmapping changer par getmapping
+	@GetMapping("/seconnecter") 
 	@ResponseBody
-	public Utilisateur getLogin(@RequestBody Utilisateur newUser) { // remplacer par String Email à la place Utilisateur newuser 
-		String email = newUser.getEmail(); // virer cette ligne ensuite // ligne 22 @requestBody remplacher par un @pathvariable 
+	public Utilisateur getLogin(@RequestParam(value="email", required = true) String email) { 
 
 		List<Utilisateur> utilisateurs = utilisateurService.getLogin(email);
 		for (Utilisateur utilisateur : utilisateurs) {
