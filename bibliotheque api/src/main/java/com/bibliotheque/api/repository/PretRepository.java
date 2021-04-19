762280ac1a315ada3ef5094285c2eb8765e2530a
alexa <alexa@DESKTOP-J22173Q>
2021-04-19 18:18:03 +0200

    correction

--- bibliotheque api/src/main/java/com/bibliotheque/api/repository/PretRepository.java
+++ bibliotheque api/src/main/java/com/bibliotheque/api/repository/PretRepository.java
@@ -6,12 +6,11 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bibliotheque.api.model.Livre;
 import com.bibliotheque.api.model.Pret;
 
 @Repository
 public interface PretRepository extends CrudRepository<Pret, Integer> {
 
-	List<Livre> findbyDateDeFinBefore(Date dateDeFin);
+	List<Pret> findbyDateDeFinBefore(Date dateDeFin);
 
 }
